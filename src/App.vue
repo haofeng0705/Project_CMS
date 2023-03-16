548b63029724113f8864edee695ff52c30044104
haofeng0705 <raphael.hao.alive@gmail.com>
2023-03-16 21:40:51 +0800

    的

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="app">
     <h2>App</h2>
     <router-link to="/main"> 主要 </router-link>---
-    <router-link to="/login"> 登录 </router-link>
+    <router-link to="/login"> 登 录 </router-link>
     <router-view></router-view>
   </div>
 </template>
